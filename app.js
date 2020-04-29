4fe5744c7060b0446a39df52243003d744caaecf
Devinhodge <d.hodge8891@yahoo.com>
2020-04-28 19:17:15 -0500

    playing with todo list

--- app.js
+++ app.js
@@ -15,15 +15,29 @@
   "completed": false
 }]
 
-console.log(arrayOfTodos[0].userId)
-console.log(arrayOfTodos[1].userId)
+console.log(arrayOfTodos[0].userId) // => 14
+console.log(arrayOfTodos[1].userId) // => 20
 
 let orderedListElement = document.querySelector('ol')
+
+listItem = document.createElement("li");
+
 const fetchTodos = () => {
   fetch('https://jsonplaceholder.typicode.com/todos')
-  .then( (response) => response.json())
-  .then( (json) => arrayOfTodos = json)
-  console.log('fetchTodos', fetchTodos)
+  .then((response) => response.json())
+  .then((json) => arrayOfTodos = json)
+  .then(json => console.log(json))
+    if (this.response === this.DONE && this.status === 200) {
+    if (this.responseText) { 
+        listItem.innerHTML = this.responseText;
+        fetchTodos(this.responseText);
+    }
+    else {
+        console.log("Error: Data is empty");
+    }
+}
+  console.log('orderedListElement', orderedListElement)
+
 }
 
 const logTodos = () => {
