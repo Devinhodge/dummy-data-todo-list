5f5c3017964039b649136325be5e427147811bcd
Devinhodge <d.hodge8891@yahoo.com>
2020-05-02 16:53:55 -0500

    got the full list to show

--- app.js
+++ app.js
@@ -1,58 +1,30 @@
- // We'll pre-populate this array with a couple objects just so it's not undefined if your internet connection isn't working properly.
-
- /*let arrayOfTodos = [
-  {
-  "userId": 14,
-  "id": 1,
-  "title": "delectus aut autem",
-  "completed": false
-},
-
-{
-  "userId": 20,
-  "id": 2,
-  "title": "delectus aut autem",
-  "completed": false
-}]
-
-console.log(arrayOfTodos[0].userId) // => 14
-console.log(arrayOfTodos[1].userId) // => 20
-*/
 let orderedListElement = document.querySelector('ol')
 
 const fetchTodos = () => {
   fetch('https://jsonplaceholder.typicode.com/todos')
-  .then( (response) => response.json())
-  .then( (json) => arrayOfTodos = json)
-      console.log('fetchTodos', fetchTodos)
+  .then( response => response.json())
+  .then( json => arrayOfTodos = json)
+    console.log('fetchTodos', fetchTodos)
 }
 const logTodos = () => {
-
   console.log(arrayOfTodos)
-
 }
-
 const populateTodos = () => {
   console.log('orderedListElement', orderedListElement)
   console.log('arrayOfTodos', arrayOfTodos)
   var listItem = document.createElement("li");
   
-  arrayOfTodos.forEach(item => {
-    console.log('item', item)
-    listItem.innerText = item.title
+  arrayOfTodos.forEach(obj => {
+    console.log('obj', obj)
+    listItem.innerText = obj.title
     orderedListElement.appendChild(listItem)
+    //changedColor(obj)
   })
+
   for (let index = 0; index < arrayOfTodos.length; index++) {
     const element = arrayOfTodos[index];
+    var listItem = document.createElement("li")
+    listItem.innerText = element.title + element.userId
+    orderedListElement.appendChild(listItem)
   }
 }
-/*let changedColor = (item) => {
-  if (item.completed === false) {
-  }
-  if (item.completed) {
-    li.style.color = 'green'
-  } else {
-    li.style.color = 'red'
-  }
-}
-//document.getElementById("todo-list").innerHTML = arrayOfTodos;*/
