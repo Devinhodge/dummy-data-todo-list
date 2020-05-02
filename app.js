01a3567144e3f39227e97ed97ef1c2eb447fb567
Devinhodge <d.hodge8891@yahoo.com>
2020-05-02 18:02:27 -0500

    made all one color, trying to get the other color

--- app.js
+++ app.js
@@ -13,18 +13,27 @@ const populateTodos = () => {
   console.log('orderedListElement', orderedListElement)
   console.log('arrayOfTodos', arrayOfTodos)
   var listItem = document.createElement("li");
-  
+ 
   arrayOfTodos.forEach(obj => {
     console.log('obj', obj)
     listItem.innerText = obj.title
     orderedListElement.appendChild(listItem)
-    //changedColor(obj)
+    changedColor(listItem)
   })
 
   for (let index = 0; index < arrayOfTodos.length; index++) {
     const element = arrayOfTodos[index];
     var listItem = document.createElement("li")
-    listItem.innerText = element.title + element.userId
+    listItem.innerText = element.title + "" + element.userId
     orderedListElement.appendChild(listItem)
+    changedColor(listItem)
+  }
+}
+
+let changedColor = (listItem) => {
+  if (listItem.completed === true) {
+    listItem.style.color = 'red'
+  } else { 
+    listItem.style.color = 'green'
   }
 }
