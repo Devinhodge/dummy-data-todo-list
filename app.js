c48fa44c4400210e4a2338ad58b52c39a8c10bbb
Devinhodge <d.hodge8891@yahoo.com>
2020-05-03 21:12:15 -0500

    finally got both false and true to show up

--- app.js
+++ app.js
@@ -1,39 +1,29 @@
-let orderedListElement = document.querySelector('ol')
-
 const fetchTodos = () => {
-  fetch('https://jsonplaceholder.typicode.com/todos')
-  .then( response => response.json())
-  .then( json => arrayOfTodos = json)
-    console.log('fetchTodos', fetchTodos)
-}
+  fetch("https://jsonplaceholder.typicode.com/todos")
+    .then((response) => response.json())
+    .then((json) => {
+      arrayOfTodos = json;
+      console.log('fetchTodos', fetchTodos)
+    });
+};
+
 const logTodos = () => {
-  console.log(arrayOfTodos)
-}
+  console.log(arrayOfTodos);
+};
+
 const populateTodos = () => {
-  console.log('orderedListElement', orderedListElement)
-  console.log('arrayOfTodos', arrayOfTodos)
-  var listItem = document.createElement("li");
- 
-  arrayOfTodos.forEach(obj => {
-    console.log('obj', obj)
-    listItem.innerText = obj.title
-    orderedListElement.appendChild(listItem)
-    changedColor(listItem)
-  })
-
-  for (let index = 0; index < arrayOfTodos.length; index++) {
-    const element = arrayOfTodos[index];
-    var listItem = document.createElement("li")
-    listItem.innerText = element.title + "" + element.userId
-    orderedListElement.appendChild(listItem)
-    changedColor(listItem)
-  }
-}
-
-let changedColor = (listItem) => {
-  if (listItem.completed === true) {
-    listItem.style.color = 'red'
-  } else { 
-    listItem.style.color = 'green'
-  }
-}
+  var ol = document.getElementById("todo-list");
+  ol.innerHTML = ``;
+  arrayOfTodos.forEach((x) => {
+    var li = document.createElement("li");
+    var span = document.createElement("span");
+    if (!x.completed) {
+      span.style = "color:red";
+    } else {
+      span.style = "color:green";
+    }
+    span.innerText = x.title;
+    li.appendChild(span);
+    ol.appendChild(li);
+  });
+};
